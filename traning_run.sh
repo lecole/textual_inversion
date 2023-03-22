c4fc13eb023fa75120968ce881f82372a8556332
Lecole Cole <lecole@gmail.com>
2023-03-21 22:47:17 -0700

    V2

--- traning_run.sh
+++ traning_run.sh
@@ -5,9 +5,9 @@ DATASET='/Users/lecole/dev/traning_images'
 LOGDIR=$(ls -td logs/* | head -1)
 
 python main.py --base configs/stable-diffusion/v1-lc-finetune-mywords.yaml \
-               -train \
+               --train \
                --actual_resume models/stable_diffusion/v1-5-pruned-emaonly.ckpt \
-               -name lc-v1 \
+               --name lc-v1 \
                --gpus 0, \
                --data_root  $DATASET \
                --seed 12345 \
